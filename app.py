e52f5bc77f987d1284aee1912052d2e9c5381f7f
Chris Wacek <cwacek@cs.georgetown.edu>
2012-12-20 22:36:39 -0500

    Yay. App.py

--- app.py
+++ app.py
@@ -180,4 +180,5 @@ def result():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug=True
     app.run(host='0.0.0.0', port=port, debug=True)
