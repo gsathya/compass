4b86e4173bbdd7ebcb26fad669bed1393fb82c87
Chris Wacek <cwacek@cs.georgetown.edu>
2012-12-23 00:35:39 -0500

    Fixed some debugging things that shouldn't be there

--- app.py
+++ app.py
@@ -174,5 +174,4 @@ def result():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.debug=True
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
